2fe07aa8f55deced93d3d63c1d943f385ad642f4
aditya-7827 <aditya07827@gmail.com>
2026-05-04 23:35:21 +0530

    second commit

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL
+    baseURL: "https://resumebuilder-server-tmfc.onrender.com"
 })
 
 export default api
